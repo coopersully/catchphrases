f503fb2f5187f1cf1765de371e7a849990b9b836
Cooper Sullivan <thecoopersully@gmail.com>
2023-07-17 17:24:30 -0500

    Move entry sorting to client-side

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, session, jsonify
-import os
 import json
-import random
+import os
+
+from flask import Flask, render_template
+from random import shuffle
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
@@ -21,48 +22,14 @@ def categories():
     return render_template('categories.html', categories=categories)
 
 
-@app.route('/category/<string:category_name>', methods=['GET', 'POST'])
+@app.route('/category/<string:category_name>', methods=['GET'])
 def category(category_name):
-    action = request.form.get('action') if request.method == 'POST' else None
     with open(f'categories/{category_name}', 'r') as f:
         category = json.load(f)
 
-    # Get previous, current and remaining phrases from session
-    previous_phrases = session.get('previous_phrases', [])
-    current_phrase = session.get('current_phrase')
-    remaining_phrases = session.get('remaining_phrases', category['entries'])
-
-    # Initial load of the category page
-    if current_phrase is None:
-        current_phrase = random.choice(remaining_phrases)
-        remaining_phrases.remove(current_phrase)
-
-    elif action == 'Next':
-        # When all phrases have been shown, reset
-        if not remaining_phrases:
-            remaining_phrases = category['entries']
-
-        # Don't add to previous_phrases on first phrase
-        if current_phrase is not None:
-            previous_phrases.append(current_phrase)
-
-        current_phrase = random.choice(remaining_phrases)
-        remaining_phrases.remove(current_phrase)
-
-    elif action == 'Previous' and previous_phrases:
-        remaining_phrases.append(current_phrase)
-        current_phrase = previous_phrases.pop()
-
-    session['previous_phrases'] = previous_phrases
-    session['current_phrase'] = current_phrase
-    session['remaining_phrases'] = remaining_phrases
-
-    # AJAX Request
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return jsonify(current_phrase=current_phrase)
-    # Normal Request
-    else:
-        return render_template('category.html', category=category, current_phrase=current_phrase)
+    # Randomize the order of entries
+    shuffle(category['entries'])
+    return render_template('category.html', category=category)
 
 
 @app.route('/times_up')
